a2c31b097f0a65466e50eba07771267d223ab749
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-01-24 09:29:56 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class Tester{
 	
 	public static void main(String[] args){
 		Mastermind m1 = new Mastermind();
 		m1.printBoard();
+		m1.board();
+		
+		System.out.println("Welcome to Mastermind!");
+		//System.out.println("Welcome to Mastermind!");
+		Scanner scan = new Scanner (System.in);
+		
+		
+		
+		
+		
 		
 		
 		
@@ -13,6 +24,8 @@ public class Tester{
 	
 }
 		
+	
+	
 	
 
 
